40124232637a9cb77a2b43ab2862d59b95af6b53
Zhenni Qian <zhenni.qian913@gmail.com>
2021-03-18 23:10:19 +0100

    added users file to implement users module, resolver and service

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,12 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { User } from './models/user';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly usersService: UsersService) {}
+  @Query(() => User, { name: 'user', nullable: true })
+  getUser(): User {
+    return this.usersService.getUser();
+  }
+}
